3c61a4512efa31d4963b6fbebed5a6c24d74c114
ANKIT RAWAT <36503727+iamrawat1995@users.noreply.github.com>
2024-10-06 11:58:33 +0000

    Added a variable file

---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 1452476
+let accountMail = "abc123@hotmail.com"
+var accountCity = "Gurgaon"
+accountIp ="172.276.287.37"
+
+accountMail = "xyz321@gmail.com"
+
+console.log(accountMail);
+console.table([accountId,accountMail,accountCity,accountIp])
