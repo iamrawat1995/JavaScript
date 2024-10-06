3703bffb7fceefa3d8b94f45ab744f891619b33b
ANKIT RAWAT <36503727+iamrawat1995@users.noreply.github.com>
2024-10-06 17:16:01 +0000

    data types summary added

---
+++ Basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+//for undefined
+
+// console.log(undefined == 0)
+// console.log(undefined >0);
+
+/// strict check with '==='
+
+console.log("2"==2)
+console.log("2"===2)
