13e45616d62e5cf83203a6696529a5a391761150
ANKIT RAWAT <36503727+iamrawat1995@users.noreply.github.com>
2024-10-06 12:15:32 +0000

    Added data types

---
+++ Basics/dataTypes.js
@@ -0,0 +1,21 @@
+let name = "Ankit"
+let age = 29
+let isLoggedIn = true
+console.log(age);
+
+
+let unknown;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+age = "King"
+console.log(age)
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
